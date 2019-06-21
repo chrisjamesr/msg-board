553c820c8f4cf21079018954c52db38e6b1eed3a
chrisjamesr <chrisjames.r@gmail.com>
2019-06-21 13:07:47 -0400

    import packages for schema

---
+++ schema.js
@@ -0,0 +1,3 @@
+const GraphQLSchema = require('graphql').GraphQLSchema;
+const makeExecutableSchema = require('graphql-tools').makeExecutableSchema;
+const resolver = require('./resolver').resolver;
