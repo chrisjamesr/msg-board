04b6d681fd1a598d16c72d5a891c54258166e14f
chrisjamesr <chrisjames.r@gmail.com>
2019-06-21 13:07:24 -0400

    add config object w/reference to env variables, comment out conn string

--- services/psqlAdapter.js
+++ services/psqlAdapter.js
@@ -1,8 +1,19 @@
 const pgPromise = require('pg-promise');
 
-const connString = 'postgresql://msg_board@localhost:5432/message_board';
+// const connString = 'postgresql://msg_board@localhost:5432/message_board';
 
 const pgp = pgPromise({});
-const psql = pgp(connString);
 
-exports.psql = psql
+const config = {
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    database: process.env.POSTGRES_DB,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    max: 10, 
+    idleTimeoutMillis: 30000,
+};
+
+const db = pgp(pgconfig);
+
+exports.db = db;
