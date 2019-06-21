9dff6755284a4bf34c6efbb524445c0af31a48e9
chrisjamesr <chrisjames.r@gmail.com>
2019-06-21 12:49:45 -0400

    Add graphql middleware

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const graphql = require('graphql');
 
-const app = express();
 const port = process.env.PORT || 3001
+const app = express();
+
+app.use('/api', graphqlHTTP({
+    schema: schema,
+    graphiql: true,
+}));
+
 
 app.get('/', (req,res) => {
     res.send('Root Route')
