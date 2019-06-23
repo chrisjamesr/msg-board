c527d8effcef73025be40e83192a658aac51ac66
chrisjamesr <chrisjames.r@gmail.com>
2019-06-23 15:11:23 -0400

    add type definitions

--- schemas/types.js
+++ schemas/types.js
@@ -1,19 +1,27 @@
-const GraphQLSchema = require('graphql').GraphQLSchema;
+const graphql = require('graphql');
+const { GraphQLObjectType, GraphQLString } = graphql;
+
 const makeExecutableSchema = require('graphql-tools').makeExecutableSchema;
 const resolver = require('../resolver').resolver;
 
-const typeDefs = `
-  type User {
-    id: String!
-    name: String
-    email: String
-  }
-  type Query {
-    users: [User]!
-  }
-`;
+const MessageType = new GraphQLObjectType({
+    name: "Message",
+    type: "Query",
+    fields: {
+        id: {type: GraphQLString },
+        content: {type: GraphQLString },
+        user: {type: GraphQLString }
+    }
+});
 
-exports.schema = makeExecutableSchema({
-  typeDefs,
-  resolver,
-  });
+const UserType = new GraphQLObjectType({
+    name: "User",
+    type: "Query",
+    fields: {
+        id: {type: GraphQLString },
+        email: {type: GraphQLString }
+    }
+});
+
+exports.MessageType = MessageType;
+exports.UserType = UserType;
