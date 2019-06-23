91f686c91438fbf24123cffa75cf3acd7e305602
chrisjamesr <chrisjames.r@gmail.com>
2019-06-23 14:40:38 -0400

    add test query

--- services/psqlAdapter.js
+++ services/psqlAdapter.js
@@ -11,10 +11,18 @@ const config = {
     database: process.env.POSTGRES_DB,
     user: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD,
-    max: 10, 
-    idleTimeoutMillis: 30000,
+    // max: 10, 
+    // idleTimeoutMillis: 30000,
 };
 
-const db = pgp(pgconfig);
+const db = pgp(config);
+console.log(config);
+
+db.manyOrNone('select * from messages')
+    .then(res=> {
+        console.log(res);
+    })
+    .catch(err=> console.log(err));
+    
 
 exports.db = db;
