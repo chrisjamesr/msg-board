768bd39923a5c92f83333baabe894c7b66dcf622
chrisjamesr <chrisjames.r@gmail.com>
2019-06-24 12:47:41 -0400

    remove log statement and return result from db call

--- schemas/queries.js
+++ schemas/queries.js
@@ -1,4 +1,25 @@
 const { db } = require('../services/psqlAdapter');
-const { graphQLObjectType, GraphQLID } = require("graphql");
+const { 
+    GraphQLObjectType, 
+    GraphQLID,
+    GraphQLList } = require("graphql");
 const { MessageType, UserType } = require('./types');
 
+const RootQuery = new GraphQLObjectType({
+    name: "RootQueryType",
+    type: "Query",
+    fields: {
+        messages: {
+            types: GraphQLList(MessageType),
+            // args: { id: { type: GraphQLID } }, 
+            resolve(parentValue, args){          
+                const query = 'SELECT * FROM messages'
+                return db.manyOrNone(query)
+                    .then(res=> res)
+                    .catch(err=> console.log(err));
+            }
+        }
+    }
+});
+
+exports.query = RootQuery;
