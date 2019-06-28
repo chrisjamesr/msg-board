3a393c3db1094127e501ad27116bd5c892534aff
chrisjamesr <chrisjames.r@gmail.com>
2019-06-28 13:03:31 -0400

    remove apollo client code

--- client/src/index.js
+++ client/src/index.js
@@ -2,11 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import 'dotenv/config';
+import 'cross-fetch/polyfill';
+
+
+
+
+
+console.log(process.env);
+
+// import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// serviceWorker.unregister();
